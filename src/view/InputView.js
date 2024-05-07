c2e80f11bf63085df33aad8fef066d9ef87f48fa
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-05-08 01:05:08 +0900

    Modify: 함수 선언 변경

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,15 +1,15 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console } from '@woowacourse/mission-utils';
 
 export default class InputView {
-  getInputMoney = async () => {
-    return Console.readLineAsync("구입금액을 입력해 주세요.\n");
-  };
+  static async getInputMoney() {
+    return Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  }
 
-  getWinningNumbers = async () => {
-    return Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
-  };
+  static async getWinningNumbers() {
+    return Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
+  }
 
-  getBonusNumber = async () => {
-    return Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
-  };
+  static async getBonusNumber() {
+    return Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
+  }
 }
