40152993ae182b1ba254f48ebf052762f3f71cd9
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-05-09 02:03:32 +0900

    Feat: 로또 당첨 번호 입력 유효성 검사 및 TDD 구현

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,17 +1,36 @@
-import Lotto from "../src/Lotto.js";
+/* eslint-disable no-new */
+import Lotto from '../src/Lotto.js';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR] 로또 번호는 6개여야 합니다.');
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호가 숫자가 아닐 경우 예외가 발생한다.', () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+      new Lotto([1, 3, 2, '굿', 4, 5]);
+    }).toThrow('[ERROR] 숫자가 아닌 값을 입력하실 수 없습니다.');
+  });
+
+  // // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+  test('로또 번호가 1이상 45이하가 아닌경우 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([0, 47, -1, 100, 120, 70]);
+    }).toThrow('[ERROR] 로또 당첨번호는 1부터 45 사이입니다.');
+  });
+
+  test('로또 당첨 번호는 중복일 수 없습니다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 4, 5]);
+    }).toThrow('[ERROR] 당첨번호는 중복된 값이 없습니다.');
+  });
+
+  test(',테스트', () => {
+    expect(() => {
+      new Lotto([, 1, 2, 3, 4, 5]);
+    }).toThrow('[ERROR]');
   });
 
   // 아래에 추가 테스트 작성 가능
--- src/Lotto.js
+++ src/Lotto.js
@@ -2,17 +2,59 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    this.validate(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
+  validate(numbers) {
+    this.checkLottoArrayLength(numbers);
+    this.checkNunNumber(numbers);
+    this.checkLottoRange(numbers);
+    this.checkDuplicateNumber(numbers);
+    this.checkEmpty(numbers);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  checkLottoArrayLength(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  // eslint-disable-next-line class-methods-use-this
+  checkNunNumber(numbers) {
+    numbers.forEach((number) => {
+      const parseNumber = Number(number);
+      if (Number.isNaN(parseNumber)) {
+        throw new Error('[ERROR] 숫자가 아닌 값을 입력하실 수 없습니다.');
+      }
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  checkLottoRange(numbers) {
+    numbers.forEach((number) => {
+      const parseNumber = Number(number);
+      if (parseNumber < 1 || parseNumber > 45) {
+        throw new Error('[ERROR] 로또 당첨번호는 1부터 45 사이입니다.');
+      }
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  checkDuplicateNumber(numbers) {
+    const dedupeNumber = new Set(numbers);
+    if (dedupeNumber.size !== numbers.length) {
+      throw new Error('[ERROR] 당첨번호는 중복된 값이 없습니다.');
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  checkEmpty(numbers) {
+    if (numbers.some((item) => item === undefined)) {
+      throw new Error('[ERROR] 당첨번호는 중복된 값이 없습니다.');
+    }
+  }
 }
 
 export default Lotto;
