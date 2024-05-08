37f41aa1b692c5ebf479b873eee06b37e12711d8
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-05-09 00:35:07 +0900

    Modify: 페어 Prettier 설정 통일 + 구현 목록 업데이트

--- __tests__/PurchaseTest.js
+++ __tests__/PurchaseTest.js
@@ -1,34 +1,39 @@
-import Purchase from "../src/Purchase.js";
+import Purchase from '../src/Purchase.js';
 
-describe("로또 구입금액 테스트", () => {
-  test("입력금액을 통해 구매수량 구하기", () => {
-    const purchase = new Purchase("10000");
+// eslint-disable-next-line max-lines-per-function
+describe('로또 구입금액 테스트', () => {
+  test('입력금액을 통해 구매수량 구하기', () => {
+    const purchase = new Purchase('10000');
     const amount = purchase.getQuantity();
     expect(amount).toBe(10);
   });
 
-  test("0이하의 숫자를 입력할 경우 에러 발생", () => {
+  test('0이하의 숫자를 입력할 경우 에러 발생', () => {
     expect(() => {
-      new Purchase("-1");
-    }).toThrow("[ERROR] 숫자가 0보다 작으면 안됩니다.");
+      // eslint-disable-next-line no-new
+      new Purchase('-1');
+    }).toThrow('[ERROR] 숫자가 0보다 작으면 안됩니다.');
   });
 
-  test("숫자가 아닌 경우 에러 발생", () => {
+  test('숫자가 아닌 경우 에러 발생', () => {
     expect(() => {
-      new Purchase("굿");
-    }).toThrow("[ERROR]");
+      // eslint-disable-next-line no-new
+      new Purchase('굿');
+    }).toThrow('[ERROR]');
   });
 
-  test("공백을 입력한 경우 에러 발생", () => {
+  test('공백을 입력한 경우 에러 발생', () => {
     expect(() => {
-      new Purchase(" ");
-    }).toThrow("[ERROR]");
+      // eslint-disable-next-line no-new
+      new Purchase(' ');
+    }).toThrow('[ERROR]');
   });
 
-  test("1000으로 나누어 떨어지지 않으면 에러 발생", () => {
+  test('1000으로 나누어 떨어지지 않으면 에러 발생', () => {
     expect(() => {
-      new Purchase("30");
-    }).toThrow("[ERROR]");
+      // eslint-disable-next-line no-new
+      new Purchase('30');
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
