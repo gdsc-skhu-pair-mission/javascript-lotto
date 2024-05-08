26850b9e5d476346164192c5cb2db7f9cec7f3c0
SEYEON-PARK <fourleaf123@naver.com>
2024-05-09 00:18:51 +0900

    Modify: 구매 금액 TDD 완성

--- __tests__/PurchaseTest.js
+++ __tests__/PurchaseTest.js
@@ -1,18 +1,34 @@
-import Purchase from '../src/Purchase.js';
+import Purchase from "../src/Purchase.js";
 
-describe('로또 구입금액 테스트', () => {
-  test('입력금액을 통해 구매수량 구하기', () => {
-    const purchase = new Purchase(10000);
+describe("로또 구입금액 테스트", () => {
+  test("입력금액을 통해 구매수량 구하기", () => {
+    const purchase = new Purchase("10000");
     const amount = purchase.getQuantity();
     expect(amount).toBe(10);
   });
 
-  test('숫자가 아닌 문자를 입력했을 때 에러발생', () => {
-    const invailedInput = '지섭햄 최고';
-    const vailedate = new Purchase(invailedInput);
+  test("0이하의 숫자를 입력할 경우 에러 발생", () => {
     expect(() => {
-      vailedate.checkNunNumber(invailedInput);
-    }).toThrow('[ERROR] 숫자가 아닌 값을 입력하실 수 없습니다.');
+      new Purchase("-1");
+    }).toThrow("[ERROR] 숫자가 0보다 작으면 안됩니다.");
+  });
+
+  test("숫자가 아닌 경우 에러 발생", () => {
+    expect(() => {
+      new Purchase("굿");
+    }).toThrow("[ERROR]");
+  });
+
+  test("공백을 입력한 경우 에러 발생", () => {
+    expect(() => {
+      new Purchase(" ");
+    }).toThrow("[ERROR]");
+  });
+
+  test("1000으로 나누어 떨어지지 않으면 에러 발생", () => {
+    expect(() => {
+      new Purchase("30");
+    }).toThrow("[ERROR]");
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
